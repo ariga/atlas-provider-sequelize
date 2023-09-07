eea6e0611a443afae05110f9b640cabf50be2d99
Ronen Lubin <ronen7822@gmail.com>
2023-09-07 10:53:49 +0300

    add ts and js basic implemation for sequlize schema loaders

---
+++ ts/src/models/Contact.ts
@@ -0,0 +1,54 @@
+import { Table, Column, Model, PrimaryKey, AutoIncrement, DataType, AllowNull, CreatedAt, UpdatedAt, DefaultScope, AfterCreate, HasMany, Length } from 'sequelize-typescript';
+import Phone from './Phone';
+import Email from './Email';
+
+@DefaultScope(() => ({
+    include: [{
+        model: Phone,
+        as: Phone.tableName
+    },{
+        model: Email,
+        as: Email.tableName
+    }],
+    attributes: {
+        exclude: ['created_at', 'updated_at']
+    }
+}))
+@Table({ tableName: 'contact' })
+class Contact extends Model {
+    @PrimaryKey
+    @AutoIncrement
+    @Column(DataType.INTEGER)
+    id!: number;
+
+    @AllowNull(false)
+    @Length({ min: 3, max: 45 })
+    @Column(DataType.STRING(45))
+    name!: string;
+
+    @AllowNull(false)
+    @Length({ min: 3, max: 45 })
+    @Column(DataType.STRING(45))
+    alias!: string;
+    
+    @HasMany(() => Phone, { onDelete: 'CASCADE' })
+    phone!: Phone[];
+
+    @HasMany(() => Email, { onDelete: 'CASCADE' })
+    email!: Email[];
+    
+    @CreatedAt
+    created_at?: Date;
+
+    @UpdatedAt
+    updated_at?: Date;
+
+    @AfterCreate
+    static excludeFields(contact: Contact) {
+        const { dataValues } = contact;
+        delete dataValues.created_at;
+        delete dataValues.updated_at;
+    }
+}
+
+export default Contact;
