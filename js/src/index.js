f227f70f049085239dcefcd5256a0a80ffae803b
Ronen Lubin <ronen7822@gmail.com>
2023-09-14 13:52:34 +0300

    add scipt that load models

--- js/src/index.js
+++ js/src/index.js
@@ -1,59 +1,30 @@
 #!/usr/bin/env node
 
-const Sequelize = require("sequelize");
-const DataTypes = require("sequelize/lib/data-types");
 const yargs = require("yargs");
 const fs = require("fs");
 const { hideBin } = require("yargs/helpers");
 const process = require("process");
 const { resolve } = require("path");
+const loadModels = require("./sequelize_schema");
 
 // get sql state of sequelize models
 const loadSQL = (relativePath, driver) => {
-  let sequelize = new Sequelize({
-    dialect: driver,
-  });
   if (!fs.existsSync(relativePath)) {
     throw new Error("path does not exist");
   }
   const absolutePath = resolve(relativePath);
   // get all models from files in models folder
   const files = fs.readdirSync(absolutePath);
-  const db = {};
+  const models = [];
   for (const file of files) {
     if (file.match(/\.js$/) !== null && file !== "index.js") {
       const name = file.replace(".js", "");
-      const m = require(absolutePath + "/" + name)(sequelize, DataTypes);
-      if (m?.name) {
-        db[m.name] = m;
-      }
+      const m = require(absolutePath + "/" + name);
+      // push model to array
+      models.push(m);
     }
   }
-  // create associations between models
-  for (const modelName of Object.keys(db)) {
-    if (db[modelName]?.associate) {
-      db[modelName].associate(db);
-    }
-  }
-
-  const models = sequelize.modelManager
-    .getModelsTopoSortedByForeignKey()
-    .reverse();
-
-  let sql = "";
-  for (const model of models) {
-    const def = sequelize.modelManager.getModel(model.name);
-    const attr = sequelize
-      .getQueryInterface()
-      .queryGenerator.attributesToSQL(def.getAttributes(), { ...def.options });
-    sql +=
-      sequelize
-        .getQueryInterface()
-        .queryGenerator.createTableQuery(def.tableName, attr, {
-          ...def.options,
-        }) + "\n";
-  }
-  return sql;
+  return loadModels(driver, ...models);
 };
 
 yargs(hideBin(process.argv))
