7b8095131308778dd5c0d3a2012c8385766d0326
Ronen Lubin <ronen7822@gmail.com>
2025-03-26 15:35:30 +0200

    run fmt

--- ts/jest.config.js
+++ ts/jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-    transform: {
-        '^.+\\.tsx?$': 'ts-jest',
-    },
-    testMatch: ['**/?(*.)+(spec|test).ts?(x)'],
+  preset: "ts-jest",
+  testEnvironment: "node",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  testMatch: ["**/?(*.)+(spec|test).ts?(x)"],
 };
